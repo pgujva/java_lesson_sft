ca3d6e50c623ecfc09c76ec1fed2229b9cd2c4cf
pgujva <pgujva@usetech.ru>
2018-02-11 14:06:58 +0300

    Добавлен тестовый класс SquareTests

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,17 +4,19 @@ public class Point {
   public static void main(String[] args) {
 
     Point p1 = new Point();
-    Point p2 = new Point();
     p1.x = 5;
     p1.y = 6;
+
+    Point p2 = new Point();
     p2.x = 2;
     p2.y = 1;
     System.out.println("Расстояние между двумя точками = " + distance(p1, p2));
 
   }
 
-  double x;
-  double y;
+  public double x;
+  public double y;
+
 
   public static double distance(Point p1, Point p2) {
     return Math.sqrt((p1.x - p2.x) * 2 + (p1.y - p2.y) * 2);
