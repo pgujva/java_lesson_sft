4f72afd0117fa9cbc7d0f7a42a0ade49fc6e26e7
pgujva <pgujva@usetech.ru>
2018-02-13 22:47:40 +0300

    Рефакторинг класса GroupDeletionTests. Перенос методов GroupDeletionTests в TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -71,4 +71,12 @@ public class TestBase {
   public void tearDown() {
     wd.quit();
   }
+
+  protected void deleteSelectedGroups() {
+      wd.findElement(By.name("delete")).click();
+  }
+
+  protected void selectGroup() {
+      wd.findElement(By.name("selected[]")).click();
+  }
 }
