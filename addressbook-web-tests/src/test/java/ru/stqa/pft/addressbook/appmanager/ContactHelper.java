2f4d79c6fb85ca70c4c91f34b80c2aa5547d79cf
pgujva <pgujva@usetech.ru>
2018-02-25 22:08:59 +0300

    Заполнение форм с почти одинаковым набором полей

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,9 +1,12 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.List;
@@ -19,14 +22,20 @@ public class ContactHelper extends HelperBase {
   }
 
 
-  public void fillAddContactForm(ContactData contactData) {
+  public void fillAddContactForm(ContactData contactData, boolean creation ) {
     type(By.name("firstname"), contactData.getFirstname());
     type(By.name("lastname"), contactData.getLastname());
     type(By.name("address"), contactData.getAddress());
     type(By.name("email"), contactData.getEmail());
     type(By.name("mobile"), contactData.getMobile());
-  }
 
+
+    if(creation) {
+      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+    } else {
+      Assert.assertFalse(isElementPresent(By.name("new_group")));
+    }
+  }
 
   public void submitAddContact() {
     click(By.xpath("//div[@id='content']/form/input[21]"));
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewModificationTests.java
@@ -8,7 +8,7 @@ public class AddNewModificationTests extends TestBase {
   @Test
   public void testAddNewModification (){
    app.getContactHelper().initAddNewModification();
-   app.getContactHelper().fillAddContactForm(new ContactData("TestFirstName1", "TestLastName1", "TestAddress1", "Test111@mail.ru", "89040000011"));
+   app.getContactHelper().fillAddContactForm(new ContactData("TestFirstName1", "TestLastName1", "TestAddress1", "Test111@mail.ru", "89040000011", null),false);
    app.getContactHelper().submitAddNewModification();
    app.getContactHelper().returnToHomePage();
   }
