89de5c913a1f1c95fadbdb3d902a1221d88e80d2
pgujva <pgujva@usetech.ru>
2018-03-04 01:45:29 +0300

    4.9 Вычисление максимальное идентификатора при помощи лямбда выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ package ru.stqa.pft.addressbook.tests;
         import org.testng.annotations.Test;
         import ru.stqa.pft.addressbook.model.GroupData;
 
+        import java.util.Comparator;
         import java.util.HashSet;
         import java.util.List;
 
@@ -15,18 +16,13 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreationTests() {
    app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
    Assert.assertEquals(after.size(),before.size() + 1);
 
-    int max = 0;
-    for (GroupData g: after) {
-      if (g.getId()>max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after) );
   }
