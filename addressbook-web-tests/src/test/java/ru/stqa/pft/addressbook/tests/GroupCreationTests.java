e1372e38357070fdef46d54ea00a9db701c98032
pgujva <pgujva@usetech.ru>
2018-03-21 23:19:02 +0300

    ДЗ№15 проверка данных из базы для тестов групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -61,10 +61,10 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreationTests(GroupData group) {
 
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(
             before.withAdded( group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
@@ -73,11 +73,11 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testBadGroupCreationTests() {
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData group = new GroupData().withName("test'");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before));
   }
 }
