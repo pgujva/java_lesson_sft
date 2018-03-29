71ecc2eb864839016121668825cc940164049337
pgujva <pgujva@usetech.ru>
2018-03-30 01:20:44 +0300

    8.6 -1

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,12 +1,44 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-  @Test
+  @BeforeMethod
+  public void startMailServer(){
+    app.mail().start();
+  }
 
-  public void testRegistration() {
-    app.registration().start("user1","usre1@localhost");
+  @Test
+  public void testRegistration() throws IOException, MessagingException {
+    String user = "user1122";
+    String password = "password";
+    String email = "user1177@localhost.localdomain";
+    app.registration().start(user, email);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    String confirmationLink = findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, password);
+    assertTrue(app.newSession().login(user,password));
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regax = VerbalExpression.regex().find("http;//").nonSpace().oneOrMore().build();
+    return regax.getText(mailMessage.text);
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void stopMailServer(){
+    app.mail().stop();
   }
 }
